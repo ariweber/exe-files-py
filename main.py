eb39dbefc09c6475b3c37fa59df6d8e43cd0a7e3
aharon weber <a.w770770@gmail.com>
2026-01-29 15:23:13 +0200

    question3

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ print(os.getcwd())
 for item in os.listdir():
     print(item)
 
+
+def move_and_list(path):
+    original = os.getcwd()
+    os.chdir(path)
+    print(os.listdir())
+    os.chdir(original)
+
    
