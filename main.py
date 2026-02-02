fa7dc7d718e41849da58eb83fa03833a5ef43ec0
aharon weber <a.w770770@gmail.com>
2026-02-02 20:29:13 +0200

    question-6

--- main.py
+++ main.py
@@ -35,3 +35,15 @@ def collect_py_files(path):
     return py_files
 
 
+def count_by_extension(path):
+    counts = {}
+    for item in os.listdir(path):
+        full_path = os.path.join(path, item)
+        if os.path.isfile(full_path):
+            ext = os.path.splitext(item)[1]
+            counts[ext] = counts.get(ext, 0) + 1
+        else:
+            sub_counts = count_by_extension(full_path)
+            for k, v in sub_counts.items():
+                counts[k] = counts.get(k, 0) + v
+    return counts
